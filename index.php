6561eecc997abd542909b637d576b74734a5adf7
SUDARAT THONGTO <s6106021420185@kmutnb.ac.th>
2020-08-29 22:52:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id="nevbar">
+            <ul>
+                <li><a href="#">Logo</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Work</a></li>
+                <li><a href="#">Activity</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+
+    </body>
+</html>
